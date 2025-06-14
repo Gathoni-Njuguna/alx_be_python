71c2f5e4d9f7e37d0870f4b61cc658378b1a3eab
Gathoni-Njuguna <esternjuguna@gmail.com>
2025-06-14 03:37:03 +0300

    library management

--- programming_paradigm/library_management.py
+++ programming_paradigm/library_management.py
@@ -22,27 +22,27 @@ class Book:
 class Library:
     def __init__(self):
          
-        self.__books = []
+        self._books = []
 
     def add_book(self,book):
-        self.__books.append(book)
+        self._books.append(book)
 
     def check_out_book(self, title):
-        for book in self.__books:
+        for book in self._books:
             if book.title == title and book.is_available():
                 book.check_out()
                 return True
         return False
 
     def return_book(self, title):
-        for book in self.__books:
+        for book in self._books:
             if book.title == title and not book.is_available():
                 book.return_book()
                 return True
         return False
 
     def list_available_books(self):
-        available_books = [book for book in self.__books if book.is_available()]
+        available_books = [book for book in self._books if book.is_available()]
         if not available_books:
             print("No books available")
         else:
